14706325639b02a926678f42aeca6b89b76b95a6
mohammadrezaie82 <mohammadrezaie1382422@gmail.com>
2025-05-11 13:16:59 +0330

    updete serve

--- api/getData.js
+++ api/getData.js
@@ -1,5 +1,26 @@
 // api/getData.js
 export default async function handler(req, res) {
-  const data = { message: "Hello from Serverless Function in Vercel!" };
+  const data = {
+    projects: [
+      {
+        id: "3",
+        title: "Build new websiteBuild new websiteBuild new websitefdssd",
+        description: "Develop and deploy new company website with modern UI.",
+        complete: true,
+      },
+      {
+        id: "478e",
+        title: "dsfg",
+        description: "gggggggggggggggggggggggggggggggggf",
+        complete: false,
+      },
+      {
+        id: "54a2",
+        title: "dsfdsfsdf",
+        description: "efrefdsfdsfwefwefwef",
+        complete: false,
+      },
+    ],
+  };
   res.status(200).json(data); // بازگشت داده‌ها به صورت JSON
 }
